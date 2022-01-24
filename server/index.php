dd23967f3a74acaa501399a5a246ce453835c36f
IshanFernando <e18098@eng.pdn.ac.lk>
2022-01-24 07:44:43 +0530

    Test db connection in server

---
+++ server/index.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "id18333488_user";
+$password = "wGskxx!o>b=8Rj8)";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected to db successfully";
+?>
