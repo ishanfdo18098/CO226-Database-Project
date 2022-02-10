3fc1f53a7911ac0031e6be412b10b7e53f1b74b6
IshanFernando <e18098@eng.pdn.ac.lk>
2022-02-10 21:32:16 +0530

    SQL for inserting students into DB

--- python_scripts/get_student_from_api.py
+++ python_scripts/get_student_from_api.py
@@ -1,10 +1,22 @@
 import requests
 import json
 
+writeFile = open("./outputs/studentData.sql", "w")
+
 apiURL = "https://people.ce.pdn.ac.lk/api/students/"
 data = json.loads(requests.get(apiURL).text)
-print("INSERT INTO names VALUES ")
+writeFile.write("INSERT INTO student VALUES\n")
+totalNumStudents = len(data)
+print(totalNumStudents)
+i = 0
 for eachStudent in data:
     eachStudent = data[eachStudent]
     if len(eachStudent['name_with_initials']) != 0:
-        print(f"('{eachStudent['name_with_initials']}'),")
+        if i != totalNumStudents - 1:
+            writeFile.write(
+                f"('{eachStudent['eNumber']}','{eachStudent['emails']['faculty']}','{eachStudent['preferred_long_name']}','{eachStudent['preferred_short_name']}','{eachStudent['name_with_initials']}','','','Computing',''),\n")
+        else:
+            writeFile.write(
+                f"('{eachStudent['eNumber']}','{eachStudent['emails']['faculty']}','{eachStudent['preferred_long_name']}','{eachStudent['preferred_short_name']}','{eachStudent['name_with_initials']}','','','Computing','');")
+    i += 1
+    print(i)
