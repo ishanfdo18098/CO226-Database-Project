dd578702fe4825235181b679a7b043b5d4412dc9
IshanFernando <e18098@eng.pdn.ac.lk>
2022-02-18 19:31:05 +0530

    php code to reset db easily

--- server/reset_db.php
+++ server/reset_db.php
@@ -1,6 +1,194 @@
--- MariaDB
+<?php
+require("./functions.php");
 
-use id18333488_site; -- dont use this on 000webhost
+$sql = "-- MariaDB
+
+-- when testing code. delete exisiting tables
+
+-- dont use this on 000webhost
+-- use id18333488_site; 
+
+drop table student_works_in;
+drop table supervises;
+drop table student_skills;
+drop table company_services;
+drop table company_awards;
+drop table requests;
+drop table guides;
+drop table instructs;
+drop table student;
+drop table supervisor;
+drop table internship;
+drop table instructor;
+drop table lecturer;
+drop table company;
+drop table department;
+drop table logged_in;
+
+create table company(
+    company_id int primary key,
+    name varchar(100),
+    email varchar(100),
+    ranking varchar(100),
+    website_url varchar(100),
+    description varchar(100),
+    address_line1 varchar(100),
+    address_line2 varchar(100),
+    address_country varchar(50),
+    address_state_province varchar(100)
+);
+
+create table supervisor ( 
+    supervisor_id int primary key,
+    name varchar(100),
+    email varchar(100),
+    password varchar(50),
+    phone_number varchar(20),
+    company_id int,
+    constraint fk_supervisor_company
+        foreign key (company_id) references company (company_id)
+);
+
+create table department (
+    dep_name varchar(100) primary key,
+    email varchar(100),
+    website_url varchar(100),
+    students_url varchar(100),
+    projects_url varchar(100)
+);
+
+
+create table student (
+    e_no varchar(9) primary key,
+    email varchar(100),
+    password varchar(50),
+    first_name varchar(100),
+    last_name varchar(100),
+    preferred_name varchar(100),
+    surname_with_initials varchar(100),
+    cv varchar(100),
+    deparment_name varchar(100),
+    constraint fk_student
+        foreign key (deparment_name) references department (dep_name)
+);
+
+create table internship (
+    internship_id int primary key,
+    company_id int,
+    time_period varchar(50),
+    mode_location varchar(50),
+    type varchar(50),
+    salary_allowance int,
+    constraint fk_internship
+        foreign key (company_id) references company (company_id)
+);
+
+create table instructor (
+    instructor_id int primary key,
+    name varchar(100),
+    email varchar(100),
+    password varchar(50),
+    phone_number varchar(15),
+    deparment_name varchar(100),
+    constraint fk_instructor
+        foreign key (deparment_name) references department (dep_name)
+);
+
+create table lecturer (
+    lecturer_id int primary key,
+    name varchar(100),
+    email varchar(100),
+    password varchar(50),
+    phone_number varchar(15),
+    deparment_name varchar(100),
+    constraint fk_lecturer
+        foreign key (deparment_name) references department (dep_name)
+);
+
+create table instructs (
+    instructor_id int,
+    e_no varchar(9),
+    primary key(instructor_id,e_no),
+    constraint fk_instructs
+        foreign key (instructor_id) references instructor (instructor_id),
+        foreign key (e_no) references student (e_no)
+);
+
+create table guides (
+    lecturer_id int,
+    e_no varchar(9),
+    primary key(lecturer_id,e_no),
+    constraint fk_guides
+        foreign key (lecturer_id) references lecturer (lecturer_id),
+        foreign key (e_no) references student (e_no)
+);
+
+create table requests (
+    internship_id int,
+    e_no varchar(9),
+    date varchar(30),
+    primary key(internship_id,e_no),
+    constraint fk_requests
+        foreign key (internship_id) references internship (internship_id),
+        foreign key (e_no) references student (e_no)
+);
+
+create table student_skills (
+    e_no varchar(9),
+    skills varchar(50),
+    primary key(skills,e_no),
+    constraint fk_skills
+        foreign key (e_no) references student (e_no)
+);
+
+create table company_services (
+    company_id int,
+    services varchar(50),
+    primary key(company_id,services),
+    constraint fk_services 
+        foreign key (company_id) references company (company_id)
+);
+
+create table company_awards (
+    company_id int,
+    awards varchar(50),
+    primary key(company_id,awards),
+    constraint fk_awards
+        foreign key (company_id) references company (company_id)
+);
+
+create table supervises (
+    supervisor_id int,
+    e_no varchar(9),
+    primary key(supervisor_id,e_no),
+    constraint fk_supervises
+        foreign key (supervisor_id) references supervisor (supervisor_id)
+);
+
+create table student_works_in (
+    e_no varchar(9) primary key,
+    internship_id int,
+    constraint fk_student_works_in
+        foreign key (e_no) references student (e_no),
+        foreign key (internship_id) references internship (internship_id)
+);
+
+create table logged_in(
+    -- this could be from any table student, instructor, supervisor, lecturer
+    email varchar(100),
+    cookie_value varchar(50),
+    expires_at date
+);
+
+-- Better to put the password in each table
+-- create table users (
+--     supervisor_id int,
+--     instructor_id int,
+--     lecturer_id int,
+--     student_enumber varchar(9),
+--     password varchar(50),
+--     primary key(supervisor_id, instructor_id, lecturer_id, student_enumber, password)
+-- );
 
 insert into department values 
 ('Department of Computer Engineering','head@ce.pdn.ac.lk','http://ce.pdn.ac.lk','http://people.ce.pdn.ac.lk','https://projects.ce.pdn.ac.lk');
@@ -249,4 +437,21 @@ insert into student_skills values
 ('E/18/155','C'),
 ('E/18/155','PHP'),
 ('E/18/155','Verilog'),
-('E/18/155','MySQL');
+('E/18/155','MySQL');";
+$conn = connectToDB();
+if ($conn->multi_query($sql)) {
+    do {
+        // Store first result set
+        if ($result = $conn->store_result()) {
+            while ($row = $result->fetch_row()) {
+                printf("%s\n", $row[0]);
+            }
+            $result->free_result();
+        }
+        // if there are more result-sets, the print a divider
+        if ($conn->more_results()) {
+            printf("-------------</br>");
+        }
+        //Prepare next result set
+    } while ($conn->next_result());
+}
