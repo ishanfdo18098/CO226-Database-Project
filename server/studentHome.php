28e8d30eaa1cd556851e3bbd37fee03dae7e4f3d
IshanFernando <e18098@eng.pdn.ac.lk>
2022-02-19 11:19:14 +0530

    add student home

---
+++ server/studentHome.php
@@ -0,0 +1,123 @@
+<?php
+require("./functions.php");
+session_start();
+$conn = connectToDB();
+printHeader();
+?>
+<html>
+
+<body>
+    <?php printNavbar();
+    if ($_SESSION['isLogged'] == false || $_SESSION['userType'] != 'student') {
+        redirectToURL("/logout.php", 0);
+    }
+    $email = $_SESSION['email'];
+    $sql = "SELECT * FROM student where email = '" . $_SESSION['email'] . "'";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<center><h3>Welcome back, " . $row['preferred_name'] . "</h3><br>";
+            echo "<h4>Your details:<h4><br>";
+            echo "<h5>E Number: " . $row['e_no'] . "<br>";
+            echo "Email: " . $row['email'] . "<br>";
+            echo "First Name: " . $row['first_name'] . "<br>";
+            echo "Surname With Initials: " . $row['surname_with_initials'] . "<br>";
+            echo "CV: " . $row['cv'] . "<br>";
+            echo "Department: " . $row['department_name'] . "<br>";
+        }
+    } else {
+        echo "0 results";
+    }
+
+    $sql = "select * from supervisor, supervises where supervises.supervisor_id = supervisor.supervisor_id and supervises.e_no = (select e_no from student where email = '" . $_SESSION['email'] . "'); ";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+        while ($row = mysqli_fetch_assoc($result)) {
+            $name = $row['name'];
+            $company_id = $row['company_id'];
+            echo ("<br>You are supervised by, $name from ");
+
+            $sql = "select * from company where company_id = '$company_id';";
+            $result = mysqli_query($conn, $sql);
+
+            if (mysqli_num_rows($result) > 0) {
+                // output data of each row
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $name = $row['name'];
+                    echo ($name);
+                }
+            } else {
+                echo "server error, inform admin";
+            }
+        }
+    } else {
+        echo "N/A";
+    }
+
+
+    $sql = "select lecturer.name from guides, lecturer,student where student.email = '$email' and student.e_no = guides.e_no and guides.lecturer_id = lecturer.lecturer_id;";
+    $result = mysqli_query($conn, $sql);
+    echo ("<br><br>You are guided by, ");
+    if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+        while ($row = mysqli_fetch_assoc($result)) {
+            $name = $row['name'];
+            echo ($name . " ");
+        }
+    } else {
+        echo "N/A";
+    }
+    $sql = "select instructor.name from student, instructs, instructor where student.email = '$email' and student.e_no = instructs.e_no and instructs.instructor_id = instructor.instructor_id;
+";
+    $result = mysqli_query($conn, $sql);
+    echo ("<br><br>You are instructed by, ");
+    if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+        while ($row = mysqli_fetch_assoc($result)) {
+            $name = $row['name'];
+            echo ($name . " ");
+        }
+    } else {
+        echo "N/A";
+    }
+
+    echo "<br><br> You are currently working in: ";
+    $sql = "select internship.name as internship_name, company.name as company_name from  student, student_works_in, internship, company where student.email = '$email' and student.e_no = student_works_in.e_no and student_works_in.internship_id = internship.internship_id and internship.company_id = company.company_id;";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+        while ($row = mysqli_fetch_assoc($result)) {
+            $intern_name = $row['internship_name'];
+            $company_name = $row['company_name'];
+            echo ($intern_name . " at " . $company_name);
+        }
+    } else {
+        echo "N/A";
+    }
+
+    echo ("<br><br>You have currently requested for, <br>");
+    $sql = "select requests.date, internship.name from internship, student, requests where student.e_no = requests.e_no and student.email = '$email' and internship.internship_id = requests.internship_id;";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+        while ($row = mysqli_fetch_assoc($result)) {
+            $date = $row['date'];
+            $intership_name = $row['name'];
+            echo ($intership_name . " on " . $date . "<br>");
+        }
+    } else {
+        echo "N/A";
+    }
+
+
+
+    ?>
+    </center>
+    <?php printFooter(); ?>
+</body>
+
+</html>
