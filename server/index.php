04fca802f7ad53a6668fcd49a344fa21ff0610eb
IshanFernando <e18098@eng.pdn.ac.lk>
2022-02-22 20:52:25 +0530

    Automatically go into profile home if session is there

--- server/index.php
+++ server/index.php
@@ -3,62 +3,75 @@ session_start();
 require("./functions.php");
 $conn = connectToDB();
 printHeader();
+if (isset($_SESSION['userType'])) {
+  $userType = $_SESSION['userType'];
+  if ($userType == "student") {
+    redirectToURL("/studentHome.php", 0);
+  } else if ($userType == "instructor") {
+    redirectToURL("/instructorHome.php", 0);
+  } else if ($userType == "lecturer") {
+    redirectToURL("/lecturerHome.php", 0);
+  } else if ($userType == "supervisor") {
+    redirectToURL("/supervisorHome.php", 0);
+  }
+} else {
 ?>
-<!-- IMPORTANT -->
-<!-- Security wasnt considered when making this site 🤣 This site could be vulnerable to many types of attacks -->
+  <!-- IMPORTANT -->
+  <!-- Security wasnt considered when making this site 🤣 This site could be vulnerable to many types of attacks -->
 
-<body>
-  <?php
-  printNavbar();
-  ?>
-  <center>
-    <h3>Welcome to Internship Allocation - Computer Engineering - University of Peradeniya</h3>
-  </center>
-  <main class="login-form">
-    <div class="cotainer">
-      <div class="row justify-content-center">
-        <div class="col-md-4">
-          <div class="card">
-            <div class="card-header">Login</div>
-            <div class="card-body">
-              <form action="/login.php" method="post">
-                <div class="form-group row">
-                  <label for="email_address" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
-                  <div class="col-md-8">
-                    <input type="text" id="email_address" class="form-control" name="email-address" required autofocus>
+  <body>
+    <?php
+    printNavbar();
+    ?>
+    <center>
+      <h3>Welcome to Internship Allocation - Computer Engineering - University of Peradeniya</h3>
+    </center>
+    <main class="login-form">
+      <div class="cotainer">
+        <div class="row justify-content-center">
+          <div class="col-md-4">
+            <div class="card">
+              <div class="card-header">Login</div>
+              <div class="card-body">
+                <form action="/login.php" method="post">
+                  <div class="form-group row">
+                    <label for="email_address" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
+                    <div class="col-md-8">
+                      <input type="text" id="email_address" class="form-control" name="email-address" required autofocus>
+                    </div>
                   </div>
-                </div>
-                <br>
+                  <br>
 
-                <div class="form-group row">
-                  <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
-                  <div class="col-md-8">
-                    <input type="password" id="password" class="form-control" name="password" required>
+                  <div class="form-group row">
+                    <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
+                    <div class="col-md-8">
+                      <input type="password" id="password" class="form-control" name="password" required>
+                    </div>
                   </div>
-                </div>
 
-                <br>
-                <div class="col-md-6 offset-md-5">
-                  <button type="submit" class="btn btn-primary">
-                    Login
-                  </button>
-                </div>
+                  <br>
+                  <div class="col-md-6 offset-md-5">
+                    <button type="submit" class="btn btn-primary">
+                      Login
+                    </button>
+                  </div>
+              </div>
+              </form>
             </div>
-            </form>
           </div>
         </div>
       </div>
+      </div>
+    </main>
+
+    <div class="container">
+      <center> default password is your e number <br>
+        example: username - e18098@eng.pdn.ac.lk <br>
+        password - e18098</center>
     </div>
-    </div>
-  </main>
 
-  <div class="container">
-    <center> default password is your e number <br>
-      example: username - e18098@eng.pdn.ac.lk <br>
-      password - e18098</center>
-  </div>
+    <?php printFooter(); ?>
+  </body>
 
-  <?php printFooter(); ?>
-</body>
-
-</html>
+  </html>
+<?php } ?>
