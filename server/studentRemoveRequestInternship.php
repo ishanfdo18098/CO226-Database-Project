194433427a10569eaa9aa148c02e81c8ec8198c9
IshanFernando <e18098@eng.pdn.ac.lk>
2022-02-22 16:59:15 +0530

    spelling mistake

--- server/studentRemoveRequestInternship.php
+++ server/studentRemoveRequestInternship.php
@@ -16,7 +16,7 @@ printHeader();
         $sql = "delete from requests where e_no = (select e_no from student where email='$email') and internship_id = $internship_id;";
 
         if ($conn->query($sql) === TRUE) {
-            echo "<h3>Request for internship successfully deleted<h3><br> You will be directed back to previoud page in 2 seconds.";
+            echo "<h3>Request for internship successfully deleted<h3><br> You will be directed back to previous page in 2 seconds.";
             redirectToURL("/studentHome.php", 2000);
         } else {
             echo "Something went wrong. Maybe you have already deleted the request for this internship. If not please contact admin<br>Error: " . $sql . "<br>" . $conn->error;
--- server/studentRequestInternship.php
+++ server/studentRequestInternship.php
@@ -17,7 +17,7 @@ printHeader();
         $sql = "insert into requests values ($internship_id,(select e_no from student where email = '$email'),'$date');";
 
         if ($conn->query($sql) === TRUE) {
-            echo "<h3>New record created successfully<h3><br> You will be directed back to previoud page in 2 seconds.";
+            echo "<h3>New record created successfully<h3><br> You will be directed back to previous page in 2 seconds.";
             redirectToURL("/studentHome.php", 2000);
         } else {
             echo "Something went wrong. Maybe you have already requested for this internship. If not please contact admin<br>Error: " . $sql . "<br>" . $conn->error;
